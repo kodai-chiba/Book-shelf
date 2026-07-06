7f128b025203ec7fbf834084078147e52d9e42ee
chiba kodai <kodaichiba3@gmail.com>
2026-07-07 08:53:55 +0900

    feat: Factory作成 #3

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'author' => fake()->name(),
+            'isbn' => fake()->unique()->numerify('#############'),
+            'publisher' => fake()->company(),
+            'description' => fake()->text(200),
+            'image_url' => fake()->imageUrl(),
+            'created_by' => User::factory(),
+        ];
+    }
+}
---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Genre>
+ */
+class GenreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->randomElement([
+                '小説',
+                'ビジネス',
+                '歴史',
+                '自己啓発',
+                '教育',
+            ]),
+        ];
+    }
+}
