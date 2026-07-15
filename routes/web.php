12e7712a141a10214fdd00c6f42bd136f7a87726
chiba kodai <kodaichiba3@gmail.com>
2026-07-16 07:37:44 +0900

    feat: ジャンルCRUD実装 #6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GenreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+    return redirect()->route('login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('genres', GenreController::class);
+
+    Route::get('/books', fn() => '書籍準備中')->name('books.index');
+    Route::get('/books/create', fn () => '書籍登録準備中')->name('books.create');
+    Route::get('/ranking', fn () => 'ランキング準備中')->name('ranking.index');
+    Route::get('/favorites', fn () => 'お気に入り準備中')->name('favorites.index');
+});
