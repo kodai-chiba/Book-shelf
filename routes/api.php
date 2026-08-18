a6978d25b78bb9ad505c21d7f4b0651dee80f8cc
chiba kodai <kodaichiba3@gmail.com>
2026-08-18 10:45:01 +0900

    feat: Sanctum認証API実装 #18

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('books', BookController::class);
+    // 認証不要
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{book}', [BookController::class, 'show']);
+
+    // Sanctum認証必須
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/books', [BookController::class, 'store']);
+        Route::put('/books/{book}', [BookController::class, 'update']);
+        Route::delete('/books/{book}', [BookController::class, 'destroy']);
+    });
 });
