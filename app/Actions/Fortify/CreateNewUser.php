003d5dba6695b75074ad0ff349c98d4470442b12
chiba kodai <kodaichiba3@gmail.com>
2026-09-07 05:58:50 +0900

    docs: README更新と会員登録メッセージの修正

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,17 +22,34 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
+        Validator::make(
+            $input,
+            [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+                'password' => $this->passwordRules(),
             ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+            [
+                'name.required' => '名前は必須です。',
+                'name.string' => '名前は文字列で入力してください。',
+                'name.max' => '名前は255文字以内で入力してください。',
+
+                'email.required' => 'メールアドレスは必須です。',
+                'email.string' => 'メールアドレスは文字列で入力してください。',
+                'email.email' => 'メールアドレスの形式が正しくありません。',
+                'email.max' => 'メールアドレスは255文字以内で入力してください。',
+                'email.unique' => 'このメールアドレスはすでに使用されています。',
+
+                'password.required' => 'パスワードは必須です。',
+                'password.confirmed' => 'パスワード確認が一致しません。',
+            ]
+        )->validate();
 
         return User::create([
             'name' => $input['name'],
